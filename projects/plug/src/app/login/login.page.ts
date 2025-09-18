6289df0d50fd16d3938fb561ef556e7aff404946
Minister <mundestephane13@gmail.com>
2025-09-18 01:24:32 +0100

    chore: Created auth pages

---
+++ projects/plug/src/app/login/login.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.page.ng.html',
+  styleUrl: './login.page.scss'
+})
+export class LoginPage {
+
+}
