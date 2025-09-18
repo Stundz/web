348efd9e84135cd306f192a997e02b8f670bbaea
Minister <mundestephane13@gmail.com>
2025-09-18 01:25:04 +0100

    chore: Created header component

---
+++ projects/plug/src/app/common/components/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.ng.html',
+  styleUrl: './header.scss'
+})
+export class Header {
+
+}
