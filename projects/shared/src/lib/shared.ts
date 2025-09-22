801f2cb8fa5817ea5106d86ae642671c37e3d32e
Minister <mundestephane13@gmail.com>
2025-09-22 03:59:13 +0100

    chore: Created the shared library

---
+++ projects/shared/src/lib/shared.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'innova-shared',
+  imports: [],
+  template: `
+    <p>
+      shared works!
+    </p>
+  `,
+  styles: ``
+})
+export class Shared {
+
+}
