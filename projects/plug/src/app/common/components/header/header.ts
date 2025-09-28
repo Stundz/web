1bbe142f5b775d5979150e81c2e09727177bf325
Minister <mundestephane13@gmail.com>
2025-09-28 03:15:04 +0100

    Implemented the header components

--- projects/plug/src/app/common/components/header/header.ts
+++ projects/plug/src/app/common/components/header/header.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.ng.html',
-  styleUrl: './header.scss'
+	selector: "app-header",
+	imports: [RouterLink, MatButtonModule],
+	templateUrl: "./header.ng.html",
+	styleUrl: "./header.scss",
 })
-export class Header {
-
-}
+export class Header {}
