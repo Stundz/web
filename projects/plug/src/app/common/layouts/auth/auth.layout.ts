8ca82cb9860bbf4cc9fcb7b86b038993ddd6335a
Minister <mundestephane13@gmail.com>
2025-09-29 01:13:36 +0100

    Revamped the auth layout

--- projects/plug/src/app/common/layouts/auth/auth.layout.ts
+++ projects/plug/src/app/common/layouts/auth/auth.layout.ts
@@ -1,10 +1,17 @@
 import { Component } from "@angular/core";
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { MatRippleModule } from "@angular/material/core";
+import { MatMenuModule } from "@angular/material/menu";
 
 @Component({
 	selector: "app-auth",
-	imports: [RouterLink, RouterLinkActive, RouterOutlet, MatRippleModule],
+	imports: [
+		RouterLink,
+		RouterLinkActive,
+		RouterOutlet,
+		MatRippleModule,
+		MatMenuModule,
+	],
 	templateUrl: "./auth.layout.ng.html",
 	styleUrl: "./auth.layout.scss",
 })
