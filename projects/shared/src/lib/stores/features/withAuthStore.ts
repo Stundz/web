d7397c0f64351b521fc02a7984a057d2619320ef
Minister <mundestephane13@gmail.com>
2025-10-04 02:29:16 +0100

    Added authentication store feature for ngrx/signals

---
+++ projects/shared/src/lib/stores/features/withAuthStore.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpContext } from "@angular/common/http";
+import { inject } from "@angular/core";
+import {
+	patchState,
+	signalStoreFeature,
+	withMethods,
+	withProps,
+	withState,
+} from "@ngrx/signals";
+import { tap } from "rxjs";
+import { ENVIRONMENT, Model } from "../../types";
+import { Cookie } from "../../services";
+import { HTTP_SKIP_ON_SERVER } from "../../contexts";
+
+export interface AuthStoreState {
+	user: Model.User | null;
+}
+
+export function withAuthStore() {
+	return signalStoreFeature(
+		withState<AuthStoreState>({ user: null }),
+		withProps((_store) => ({
+			_http$: inject(HttpClient),
+			_environment: inject(ENVIRONMENT),
+			_cookies: inject(Cookie),
+		})),
+		withMethods((store) => ({
+			getUser: () =>
+				store._http$
+					.get<Model.User>(`${store._environment.url.api}/user`, {
+						context: new HttpContext().set(HTTP_SKIP_ON_SERVER, true),
+					})
+					.pipe(tap((user) => patchState(store, { user }))),
+
+			_logout: () =>
+				store._http$
+					.get<void>(`${store._environment.url.api}/auth/logout`)
+					.pipe(tap(() => store._cookies.clear())),
+		})),
+	);
+}
