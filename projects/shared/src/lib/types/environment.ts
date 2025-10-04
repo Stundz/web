60ebfeea47bc286dd3785061501125caadebe1b4
Minister <mundestephane13@gmail.com>
2025-10-04 02:29:34 +0100

    Added types for Global Environments

---
+++ projects/shared/src/lib/types/environment.ts
@@ -0,0 +1,14 @@
+import { InjectionToken } from "@angular/core";
+
+const domain = "innova.com";
+
+export interface ENVIRONMENT extends Record<string, any> {
+	url: {
+		base: string;
+		plug: string;
+		api: string;
+	};
+	production: boolean;
+}
+
+export const ENVIRONMENT = new InjectionToken<ENVIRONMENT>("ENVIRONMENT");
