749025128d4c4a44a189cedb9eb4cb5b87786be8
Minister <mundestephane13@gmail.com>
2025-10-10 16:54:50 +0100

    Added the user store to the auth layout

--- projects/plug/src/app/common/layouts/auth/auth.layout.ts
+++ projects/plug/src/app/common/layouts/auth/auth.layout.ts
@@ -1,18 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { MatRippleModule } from "@angular/material/core";
 import { MatMenuModule } from "@angular/material/menu";
+import { UserStore } from "../../stores/user";
 
 @Component({
 	selector: "app-auth",
-	imports: [
-		RouterLink,
-		RouterLinkActive,
-		RouterOutlet,
-		MatRippleModule,
-		MatMenuModule,
-	],
+	imports: [RouterLink, RouterOutlet, MatRippleModule, MatMenuModule],
 	templateUrl: "./auth.layout.ng.html",
 	styleUrl: "./auth.layout.scss",
 })
-export class AuthLayout {}
+export class AuthLayout {
+	protected readonly userStore = inject(UserStore);
+
+	constructor() {
+		this.userStore.getUser().subscribe();
+	}
+
+	ngOnInit() {}
+}
