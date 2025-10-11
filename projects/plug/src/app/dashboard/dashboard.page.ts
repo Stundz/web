3f4ec46f6983518a95f67f6f2714cc6223409eef
Minister <mundestephane13@gmail.com>
2025-10-11 04:25:02 +0100

    feat: Revamped the UI of the current dashboard

--- projects/plug/src/app/dashboard/dashboard.page.ts
+++ projects/plug/src/app/dashboard/dashboard.page.ts
@@ -1,10 +1,30 @@
+import { httpResource } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+import { environment } from "../../environments/environment";
 
 @Component({
 	selector: "app-dashboard",
-	imports: [MatButtonModule],
+	imports: [MatButtonModule, RouterLink],
 	templateUrl: "./dashboard.page.ng.html",
 	styleUrl: "./dashboard.page.scss",
 })
-export class DashboardPage {}
+export class DashboardPage {
+	sessions = httpResource(
+		() => `${environment.url.api}/plug/tutorials/sessions`,
+	);
+	pastQuestions = httpResource<{ data: Array<any> }>(
+		() => ({
+			url: `${environment.url.api}/plug/past-questions`,
+			params: {
+				limit: 3,
+			},
+		}),
+		{
+			defaultValue: {
+				data: [],
+			},
+		},
+	);
+}
