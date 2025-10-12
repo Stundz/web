b67454c5b807d4d5c6834d0d2b5a9e31f3b88a2c
Minister <mundestephane13@gmail.com>
2025-10-13 00:01:39 +0100

    feat: Revamped the homepage

--- projects/plug/src/app/home/home.page.ts
+++ projects/plug/src/app/home/home.page.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.page.ng.html',
-  styleUrl: './home.page.scss'
+	selector: "app-home",
+	imports: [MatButtonModule, RouterLink],
+	templateUrl: "./home.page.ng.html",
+	styleUrl: "./home.page.scss",
 })
-export class HomePage {
-
-}
+export class HomePage {}
