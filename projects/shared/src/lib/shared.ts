a1814531217487a9cfd9e0d4f85a52169d1f5936
Minister <mundestephane13@gmail.com>
2025-10-12 02:58:40 +0100

    feat: Added the shared library

---
+++ projects/shared/src/lib/shared.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stundz-shared',
+  imports: [],
+  template: `
+    <p>
+      shared works!
+    </p>
+  `,
+  styles: ``
+})
+export class Shared {
+
+}
