7a082993c887e4d1a85ba1d4ec0778913487098e
Minister <mundestephane13@gmail.com>
2025-10-12 01:03:52 +0100

    fix: Updated the past questions index view

--- projects/plug/src/app/past-questions/index/index.page.ts
+++ projects/plug/src/app/past-questions/index/index.page.ts
@@ -1,71 +1,87 @@
+import { ViewportScroller } from "@angular/common";
 import { httpResource } from "@angular/common/http";
-import { Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { Component, DestroyRef, inject } from "@angular/core";
+import { takeUntilDestroyed, toSignal } from "@angular/core/rxjs-interop";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatTabsModule } from "@angular/material/tabs";
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import {
+	combineLatest,
+	debounceTime,
+	distinctUntilChanged,
+	map,
+	mergeWith,
+	startWith,
+	tap,
+} from "rxjs";
 import { Model, Paginated } from "shared";
+import { PastQuestion } from "../../common/services/past-question";
 
 @Component({
 	selector: "app-index",
-	imports: [RouterLink],
+	imports: [
+		RouterLink,
+		ReactiveFormsModule,
+		MatPaginatorModule,
+		MatTabsModule,
+		MatButtonModule,
+		MatExpansionModule,
+	],
 	templateUrl: "./index.page.ng.html",
 	styleUrl: "./index.page.scss",
+	providers: [PastQuestion],
 })
 export class IndexPage {
-	// pastQuestions = [
-	// 	{
-	// 		course_code: "CEC218",
-	// 		course_title: "Introduction to Machine Learning",
-	// 		year: 2024,
-	// 		intitution: "University of Buea",
-	// 	},
-	// 	{
-	// 		course_code: "CEC318",
-	// 		course_title: "Introduction to Cloud Computing",
-	// 		year: 2025,
-	// 		intitution: "University of Buea",
-	// 	},
-	// 	{
-	// 		course_code: "CEC223",
-	// 		course_title: "Algorithms and Complexity",
-	// 		year: 2021,
-	// 		intitution: "University of Buea",
-	// 	},
-	// 	{
-	// 		course_code: "CEC218",
-	// 		course_title: "Introduction to Machine Learning",
-	// 		year: 2024,
-	// 		intitution: "University of Buea",
-	// 	},
-	// 	{
-	// 		course_code: "CEC218",
-	// 		course_title: "Introduction to Machine Learning",
-	// 		year: 2024,
-	// 		intitution: "University of Buea",
-	// 	},
-	// 	{
-	// 		course_code: "CEC218",
-	// 		course_title: "Introduction to Machine Learning",
-	// 		year: 2024,
-	// 		intitution: "University of Buea",
-	// 	},
-	// 	{
-	// 		course_code: "CEC218",
-	// 		course_title: "Introduction to Machine Learning",
-	// 		year: 2024,
-	// 		intitution: "University of Buea",
-	// 	},
-	// ];
+	private _fb = inject(FormBuilder);
+	private _route = inject(ActivatedRoute);
+	private _pastQuestionService = inject(PastQuestion);
+	private _destroyRef = inject(DestroyRef);
+	viewPortScroller = inject(ViewportScroller);
 
-	pastQuestions = httpResource<Paginated<Model.PastQuestion>>(
-		() => ({ url: "http://api.innova.localhost/plug/past-questions" }),
-		{
-			defaultValue: {
-				data: [],
-				meta: {
-					total: 0,
-					current: 0,
-				},
-				links: {},
-			},
-		},
-	);
+	form = this._fb.group({
+		q: this._fb.control<string>(this._route.snapshot.params["q"] ?? "", {
+			nonNullable: true,
+		}),
+		filters: this._fb.group({
+			page: this._fb.control<number>(0, { nonNullable: true }),
+			limit: this._fb.control<number>(0, { nonNullable: true }),
+			institution: this._fb.control<string>("", {
+				nonNullable: true,
+			}),
+			semester: this._fb.control<string>("", { nonNullable: true }),
+		}),
+	});
+
+	pastQuestions = this._pastQuestionService.pastQuestions;
+
+	constructor() {
+		this.form.controls.q.valueChanges
+			.pipe(
+				takeUntilDestroyed(this._destroyRef),
+				debounceTime(600),
+				distinctUntilChanged(),
+				map((q) => q.trim()),
+				tap((q) =>
+					this._pastQuestionService.filters.update((value) => ({
+						...value,
+						page: 1,
+						q,
+					})),
+				),
+				mergeWith(
+					this.form.controls.filters.valueChanges.pipe(
+						map((filters) =>
+							this._pastQuestionService.filters.update((value) => ({
+								...value,
+								...filters,
+							})),
+						),
+					),
+				),
+			)
+			.subscribe();
+	}
 }
