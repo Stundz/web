56f8b1fb0af543091eae58aeb353efdf11f5e218
Minister <mundestephane13@gmail.com>
2025-10-12 23:26:40 +0100

    feat: Added meta tags to the application

--- projects/plug/src/app/app.ts
+++ projects/plug/src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from "@angular/core";
+import { Meta } from "@angular/platform-browser";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
@@ -9,4 +10,32 @@ import { RouterOutlet } from "@angular/router";
 })
 export class App {
 	protected readonly title = signal("plug");
+
+	constructor(meta: Meta) {
+		meta.addTags([
+			{
+				id: "description",
+				name: "description",
+				content:
+					"A platform that helps students better thier grades and overcome academic challenges",
+			},
+			{
+				id: "og:description",
+				property: "og:description",
+				content:
+					"A platform that helps students better thier grades and overcome academic challenges",
+			},
+			{
+				id: "keywords",
+				name: "keywords",
+				content: "plug, stundz, academic, study, tutorials, past questions,",
+			},
+			{
+				id: "og:image",
+				property: "og:image",
+				content:
+					"https://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVf0U_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
+			},
+		]);
+	}
 }
