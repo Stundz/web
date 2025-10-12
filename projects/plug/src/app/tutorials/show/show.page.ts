bcfdae8c25d120f14b69ffc0a86045e36ce7b1e5
Minister <mundestephane13@gmail.com>
2025-10-12 01:12:05 +0100

    fix: Updated page to view a tutorial with its details

--- projects/plug/src/app/tutorials/show/show.page.ts
+++ projects/plug/src/app/tutorials/show/show.page.ts
@@ -1,14 +1,22 @@
 import { CurrencyPipe } from "@angular/common";
+import { HttpContext, httpResource } from "@angular/common/http";
 import { Component, input } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { environment } from "../../../environments/environment";
+import { HTTP_SKIP_ON_SERVER, Model } from "shared";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { RouterLink } from "@angular/router";
 
 @Component({
 	selector: "app-show",
-	imports: [MatButtonModule, CurrencyPipe],
+	imports: [MatButtonModule, CurrencyPipe, MatProgressBarModule, RouterLink],
 	templateUrl: "./show.page.ng.html",
 	styleUrl: "./show.page.scss",
 })
 export class ShowPage {
 	id = input.required({ alias: "tutorial" });
-	// fetch tutorial with hhtpResource
+	tutorial = httpResource<Model.Tutorial>(() => ({
+		url: `${environment.url.api}/plug/tutorial/${this.id()}`,
+		context: new HttpContext().set(HTTP_SKIP_ON_SERVER, true),
+	}));
 }
