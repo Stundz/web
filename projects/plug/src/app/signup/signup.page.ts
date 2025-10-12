6654a08b66a4f4ac7f7a2b3a315945d4da904310
Minister <mundestephane13@gmail.com>
2025-10-12 21:30:02 +0100

    fix: Migrated signup page components to that of angular material

--- projects/plug/src/app/signup/signup.page.ts
+++ projects/plug/src/app/signup/signup.page.ts
@@ -1,11 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
 	selector: "app-signup",
-	imports: [RouterLink, MatButtonModule],
+	imports: [RouterLink, MatButtonModule, MatInputModule, ReactiveFormsModule],
 	templateUrl: "./signup.page.ng.html",
 	styleUrl: "./signup.page.scss",
 })
-export class SignupPage {}
+export class SignupPage {
+	private _fb = inject(FormBuilder);
+
+	form = this._fb.group({
+		first_name: this._fb.control("", { nonNullable: true }),
+		last_name: this._fb.control("", { nonNullable: true }),
+		email: this._fb.control("", {
+			nonNullable: true,
+			validators: [Validators.required, Validators.email],
+		}),
+		password: this._fb.control("", {
+			nonNullable: true,
+			validators: [Validators.required, Validators.minLength(8)],
+		}),
+		password_confirmation: this._fb.control("", {
+			nonNullable: true,
+			validators: [Validators.required, Validators.minLength(8)],
+		}),
+	});
+}
