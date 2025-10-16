240480c0269f9c0748f73ab3b969b3053e6e7abc
Minister <mundestephane13@gmail.com>
2025-10-16 02:12:29 +0100

    fix: Made the sidebar to open and close on pc and mobile

--- projects/plug/src/app/common/layouts/auth/auth.layout.ts
+++ projects/plug/src/app/common/layouts/auth/auth.layout.ts
@@ -3,10 +3,19 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { MatRippleModule } from "@angular/material/core";
 import { MatMenuModule } from "@angular/material/menu";
 import { UserStore } from "../../stores/user";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @Component({
 	selector: "app-auth",
-	imports: [RouterLink, RouterOutlet, MatRippleModule, MatMenuModule],
+	imports: [
+		RouterLink,
+		RouterOutlet,
+		MatRippleModule,
+		MatMenuModule,
+		MatToolbarModule,
+		MatSidenavModule,
+	],
 	templateUrl: "./auth.layout.ng.html",
 	styleUrl: "./auth.layout.scss",
 })
