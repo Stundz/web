14e8ec0366fbf31996d6795e9f62331bc9711fe5
Minister <mundestephane13@gmail.com>
2025-10-16 02:41:37 +0100

    feat(plug): Migrated settings page to use material compoennts

--- projects/plug/src/app/settings/settings.page.ts
+++ projects/plug/src/app/settings/settings.page.ts
@@ -1,10 +1,21 @@
 import { Component } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
 import { MatTabsModule } from "@angular/material/tabs";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { Dropzone } from "shared";
 
 @Component({
 	selector: "app-settings",
-	imports: [MatTabsModule, MatButtonModule],
+	imports: [
+		MatTabsModule,
+		MatButtonModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatSelectModule,
+		Dropzone,
+	],
 	templateUrl: "./settings.page.ng.html",
 	styleUrl: "./settings.page.scss",
 })
