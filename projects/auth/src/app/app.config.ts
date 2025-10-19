7b018f7c12a31e3206ba595db8a71e158f474475
Minister <mundestephane13@gmail.com>
2025-10-19 03:50:23 +0100

    feat: Generated the auth app

--- projects/auth/src/app/app.config.ts
+++ projects/auth/src/app/app.config.ts
@@ -1,13 +1,33 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+	ApplicationConfig,
+	provideBrowserGlobalErrorListeners,
+	provideZonelessChangeDetection,
+} from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from "./app.routes";
+import {
+	provideClientHydration,
+	withEventReplay,
+} from "@angular/platform-browser";
+import {
+	provideHttpClient,
+	withFetch,
+	withInterceptors,
+} from "@angular/common/http";
+import { ENVIRONMENT, stundzInterceptor } from "shared";
+import { environment } from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideZonelessChangeDetection(),
+		provideRouter(routes),
+		provideClientHydration(withEventReplay()),
+		provideHttpClient(withFetch(), withInterceptors([stundzInterceptor])),
+		{
+			provide: ENVIRONMENT,
+			useValue: environment,
+		},
+	],
 };
