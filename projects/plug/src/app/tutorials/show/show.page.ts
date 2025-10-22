96435041dca12c5f429dc529c29a2194516c4a6e
Minister <mundestephane13@gmail.com>
2025-10-22 01:12:10 +0100

    fix: Show database informations on tutorials/show page

--- projects/plug/src/app/tutorials/show/show.page.ts
+++ projects/plug/src/app/tutorials/show/show.page.ts
@@ -1,15 +1,21 @@
-import { CurrencyPipe } from "@angular/common";
-import { HttpContext, httpResource } from "@angular/common/http";
-import { Component, input } from "@angular/core";
+import { CurrencyPipe, DatePipe } from "@angular/common";
+import { httpResource } from "@angular/common/http";
+import { Component, computed, input } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { environment } from "../../../environments/environment";
-import { HTTP_SKIP_ON_SERVER, Model } from "shared";
+import { Model } from "shared";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { RouterLink } from "@angular/router";
 
 @Component({
 	selector: "app-show",
-	imports: [MatButtonModule, CurrencyPipe, MatProgressBarModule, RouterLink],
+	imports: [
+		MatButtonModule,
+		CurrencyPipe,
+		MatProgressBarModule,
+		RouterLink,
+		DatePipe,
+	],
 	templateUrl: "./show.page.ng.html",
 	styleUrl: "./show.page.scss",
 })
@@ -17,6 +23,6 @@ export class ShowPage {
 	id = input.required({ alias: "tutorial" });
 	tutorial = httpResource<Model.Tutorial>(() => ({
 		url: `https://api.${environment.domain}/plug/tutorial/${this.id()}`,
-		context: new HttpContext().set(HTTP_SKIP_ON_SERVER, true),
+		// context: new HttpContext().set(HTTP_SKIP_ON_SERVER, true),
 	}));
 }
