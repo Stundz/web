4d79192827f8fd24bcf3c4056482722f0b84b4cf
Minister <mundestephane13@gmail.com>
2025-10-25 13:18:13 +0100

    feat(plug): Added component to display past tutorial session

--- projects/plug/src/app/common/services/tutorial.ts
+++ projects/plug/src/app/common/services/tutorial.ts
@@ -13,21 +13,24 @@ export class Tutorial {
 
 	create(
 		payload: Pick<
-			Model.Tutorial,
-			| "name"
-			| "description"
-			| "duration"
-			| "objectives"
-			| "course_id"
-			| "day"
-			| "time"
-			| "price"
+			Model.Plug.Tutorial,
+			"name" | "description" | "course_id" | "price"
 		> & {
 			cover: File | undefined;
 		},
 	) {
 		return this._http.post<void>(
 			`https://api.${environment.domain}/plug/tutorial`,
+			toFormData(payload),
+		);
+	}
+
+	addSession(
+		tutorial: Model.Plug.Tutorial["id"],
+		payload: Pick<Model.Plug.Session, "day" | "duration" | "objectives">,
+	) {
+		return this._http.post<void>(
+			`https://api.${environment.domain}/plug/tutorial/${tutorial}/session`,
 			toFormData(payload),
 		);
 	}
