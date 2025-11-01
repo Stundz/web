ee95886b40de2c1d9b3a48c2aa083f0671a6ef32
Minister <mundestephane13@gmail.com>
2025-11-01 04:56:16 +0100

    fix(plug): Auth layout now has dropdown below user icon

--- projects/plug/src/app/common/layouts/auth/auth.layout.ts
+++ projects/plug/src/app/common/layouts/auth/auth.layout.ts
@@ -1,11 +1,13 @@
-import { Component, input } from "@angular/core";
+import { Component, DestroyRef, inject, input } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { MatRippleModule } from "@angular/material/core";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatBadgeModule } from "@angular/material/badge";
-import { Model } from "shared";
+import { Model, User } from "shared";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
 	selector: "plug-auth-layout",
@@ -17,10 +19,21 @@ import { Model } from "shared";
 		MatToolbarModule,
 		MatSidenavModule,
 		MatBadgeModule,
+		AsyncPipe,
 	],
 	templateUrl: "./auth.layout.ng.html",
 	styleUrl: "./auth.layout.scss",
 })
 export class AuthLayout {
 	user = input.required<Model.User | undefined>();
+
+	private _userService = inject(User);
+	private _destroyRef = inject(DestroyRef);
+
+	logout() {
+		return this._userService
+			.logout()
+			.pipe(takeUntilDestroyed(this._destroyRef))
+			.subscribe();
+	}
 }
