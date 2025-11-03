85c91027a99097cd6ded7fff25975fa84c1636d2
Minister <mundestephane13@gmail.com>
2025-11-03 09:24:45 +0100

    fix: Updated Seo metadata for the tutorials page

--- projects/plug/src/app/tutorials/index/index.page.ts
+++ projects/plug/src/app/tutorials/index/index.page.ts
@@ -3,9 +3,11 @@ import { Component, inject, input } from "@angular/core";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import { MatExpansionPanel } from "@angular/material/expansion";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatTableModule } from "@angular/material/table";
+import { Meta } from "@angular/platform-browser";
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { debounceTime, distinctUntilChanged, map, mergeWith, tap } from "rxjs";
 import type { Model, Paginated } from "shared";
@@ -19,6 +21,7 @@ import type { Model, Paginated } from "shared";
 		ReactiveFormsModule,
 		MatExpansionPanel,
 		MatPaginatorModule,
+		MatCardModule,
 		CurrencyPipe,
 	],
 	templateUrl: "./index.page.ng.html",
@@ -76,4 +79,45 @@ export class IndexPage {
 				}),
 			),
 	);
+
+	constructor(meta: Meta) {
+		meta.addTags([
+			{
+				id: "description",
+				name: "description",
+				content:
+					"Search and filter through a wide range of tutorials to find the perfect tutor for you.",
+			},
+			{
+				id: "og:description",
+				property: "og:description",
+				content:
+					"Search and filter through a wide range of tutorials to find the perfect tutor for you",
+			},
+			{
+				id: "keywords",
+				name: "keywords",
+				content:
+					"plug, stundz, study, tutorials, tutor, past questions, revision, education",
+			},
+			{
+				id: "og:image",
+				property: "og:image",
+				content:
+					"https://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVf0U_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
+			},
+			{
+				id: "og:image.url",
+				property: "og:image",
+				content:
+					"http://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVf0U_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
+			},
+			{
+				id: "og:image.secure_url",
+				property: "og:image",
+				content:
+					"http://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVf0U_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
+			},
+		]);
+	}
 }
