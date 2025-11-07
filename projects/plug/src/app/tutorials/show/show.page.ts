576641effbcd2879ef9958603e91924247de338b
Minister <mundestephane13@gmail.com>
2025-11-07 14:34:17 +0100

    feat(plug): Implemented a booking form for tutorials

--- projects/plug/src/app/tutorials/show/show.page.ts
+++ projects/plug/src/app/tutorials/show/show.page.ts
@@ -1,11 +1,13 @@
 import { CurrencyPipe, DatePipe } from "@angular/common";
-import { Component, computed, input } from "@angular/core";
+import { Component, computed, inject, input } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { RouterLink } from "@angular/router";
 import { addMinutes } from "date-fns";
 import { Model } from "shared";
 import { TutorialSessions } from "../../common/components/tutorial-sessions/tutorial-sessions";
+import { MatDialog } from "@angular/material/dialog";
+import { BookingForm } from "../../common/components/booking-form/booking-form";
 
 @Component({
 	selector: "plug-show-tutorial",
@@ -24,10 +26,16 @@ export class ShowPage {
 	tutorial = input.required<Model.Plug.Tutorial>();
 	user = input.required<Model.User | undefined>();
 
+	#dialog = inject(MatDialog);
+
 	endTime = computed(() => {
 		return addMinutes(
 			this.tutorial()?.session?.day || new Date(),
 			this.tutorial()?.session?.duration || 0,
 		);
 	});
+
+	book() {
+		this.#dialog.open(BookingForm);
+	}
 }
