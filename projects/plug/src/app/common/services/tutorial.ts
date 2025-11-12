b0b1452ba839bf181ce1708f3bad7279a397cc0c
Minister <mundestephane13@gmail.com>
2025-11-12 05:21:24 +0100

    fix(plug): Revamped the tutorials service

--- projects/plug/src/app/common/services/tutorial.ts
+++ projects/plug/src/app/common/services/tutorial.ts
@@ -1,23 +1,38 @@
 import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { Model, Paginated, toFormData } from "shared";
+import { BehaviorSubject, shareReplay, startWith, switchMap, tap } from "rxjs";
 
 @Injectable()
 export class Tutorial {
 	private readonly _http = inject(HttpClient);
+	filters = new BehaviorSubject<Record<string, any>>({});
+	filters$ = this.filters.asObservable().pipe(shareReplay());
 
-	tutorials$ = this._http.get<Paginated<Model.Plug.Tutorial>>(
-		`${environment.production ? "https" : "http"}://api.${environment.domain}/plug/tutorials`,
+	tutorials$ = this.filters$.pipe(
+		switchMap((filters) => {
+			const params = Object.fromEntries(
+				Object.entries(filters).filter(([key, value]) =>
+					key === "page" && value == 1 ? false : Boolean(value),
+				),
+			);
+
+			return this._http.get<Paginated<Model.Plug.Tutorial>>(
+				`${environment.production ? "https" : "http"}://api.${environment.domain}/plug/tutorials`,
+				{
+					params,
+				},
+			);
+		}),
+		shareReplay(),
 	);
 
 	create(
 		payload: Pick<
 			Model.Plug.Tutorial,
 			"name" | "description" | "course_id" | "price"
-		> & {
-			cover: File | undefined;
-		},
+		>,
 	) {
 		return this._http.post<void>(
 			`https://api.${environment.domain}/plug/tutorial`,
