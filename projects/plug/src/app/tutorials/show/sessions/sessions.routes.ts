7631fe5d4dfd5fdae53367fec063abadd11f78ac
Minister <mundestephane13@gmail.com>
2025-11-12 10:36:59 +0100

    chore(plug): Fixed session routes

--- projects/plug/src/app/tutorials/show/sessions/sessions.routes.ts
+++ projects/plug/src/app/tutorials/show/sessions/sessions.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from "@angular/router";
-import { Tutorial } from "../../../common/services/tutorial";
 
 export const routes: Routes = [
 	{
 		path: "sessions",
+		// providers: [Session],
+		resolve: {
+			// sessions: sessionsResolver,
+		},
 		children: [
 			{
 				path: "",
 				pathMatch: "full",
 				title: "Your tutorials sessions",
+				resolve: {},
 				loadComponent: () =>
 					import("./index/index.page").then((m) => m.IndexPage),
 			},
@@ -16,6 +20,23 @@ export const routes: Routes = [
 				path: "create",
 				loadComponent: () =>
 					import("./create/create.page").then((m) => m.CreatePage),
+			},
+		],
+	},
+	{
+		path: "session/:session",
+		resolve: {
+			// session: sessionResolver,
+		},
+		children: [
+			{
+				path: "",
+				pathMatch: "full",
+				loadComponent: () => import("./show/show.page").then((m) => m.ShowPage),
+			},
+			{
+				path: "edit",
+				loadComponent: () => import("./edit/edit.page").then((m) => m.EditPage),
 			},
 		],
 	},
