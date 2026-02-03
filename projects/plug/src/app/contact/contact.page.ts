60292ae1897b0883a87227b6a2696e0efd119a5a
Minister <mundestephane13@gmail.com>
2026-02-03 13:38:36 +0100

    fix: Added contact page

---
+++ projects/plug/src/app/contact/contact.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'plug-contact',
+  imports: [],
+  templateUrl: './contact.page.ng.html',
+  styleUrl: './contact.page.scss',
+})
+export class ContactPage {
+
+}
