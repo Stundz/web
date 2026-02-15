b77ca881a54da1f03b6f06c88ed356d17770916b
Minister <mundestephane13@gmail.com>
2026-02-15 01:40:45 +0100

    fix: Revamped contact page

--- projects/plug/src/app/contact/contact.page.ts
+++ projects/plug/src/app/contact/contact.page.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
-  selector: 'plug-contact',
-  imports: [],
-  templateUrl: './contact.page.ng.html',
-  styleUrl: './contact.page.scss',
+	selector: "plug-contact",
+	imports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule],
+	templateUrl: "./contact.page.ng.html",
+	styleUrl: "./contact.page.scss",
 })
-export class ContactPage {
-
-}
+export class ContactPage {}
