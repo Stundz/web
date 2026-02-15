dafced5ebb0dd3db793b31c63247bf7bdc2ca725
Minister <mundestephane13@gmail.com>
2026-02-15 01:40:27 +0100

    chore: Revamped about page

--- projects/plug/src/app/about/about.page.ts
+++ projects/plug/src/app/about/about.page.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import { MatButton } from "@angular/material/button";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
 	selector: "plug-about",
-	imports: [RouterLink, MatButton],
+	imports: [RouterLink, MatButtonModule, MatCardModule],
 	templateUrl: "./about.page.ng.html",
 	styleUrl: "./about.page.scss",
 })
