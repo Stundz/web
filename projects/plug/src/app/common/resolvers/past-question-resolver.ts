92fb7fd02a8d7a5879664a616a6bc86885c82041
Minister <mundestephane13@gmail.com>
2026-02-22 19:27:45 +0100

    fix(plug): Title and meta tags settings moved to resolvers

--- projects/plug/src/app/common/resolvers/past-question-resolver.ts
+++ projects/plug/src/app/common/resolvers/past-question-resolver.ts
@@ -2,10 +2,11 @@ import { inject, PLATFORM_ID } from "@angular/core";
 import { ResolveFn, Router } from "@angular/router";
 import { Model, Paginated } from "shared";
 import { PastQuestion } from "../services/past-question";
-import { catchError, EMPTY, of, throwError } from "rxjs";
+import { catchError, EMPTY, of, tap, throwError } from "rxjs";
 import { isPlatformServer } from "@angular/common";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
+import { Meta, Title } from "@angular/platform-browser";
 
 export const pastQuestionsResolver: ResolveFn<
 	Paginated<Model.Plug.PastQuestion>
@@ -35,12 +36,31 @@ export const pastQuestionResolver: ResolveFn<Model.Plug.PastQuestion> = (
 ) => {
 	const http = inject(HttpClient);
 	const router = inject(Router);
+	const title = inject(Title);
+	const meta = inject(Meta);
 
 	return http
 		.get<Model.Plug.PastQuestion>(
 			`https://api.${environment.domain}/plug/past-question/${route.params["past-question"]}`,
 		)
 		.pipe(
+			tap((question) => {
+				const pageTitle = `${question.course.code} ${question.course.title}, ${question.year}`;
+				const description = `${question.course.code} ${question.course?.title}. Past question and solutions.`;
+				title.setTitle(pageTitle);
+
+				meta.updateTag({
+					id: "og:title",
+					property: "og:title",
+					content: pageTitle,
+				});
+
+				meta.updateTag({
+					id: "og:description",
+					property: "og:description",
+					content: description,
+				});
+			}),
 			catchError((response: HttpErrorResponse) => {
 				if (response.status === 404) {
 					router.navigateByUrl("/past-question/**");
