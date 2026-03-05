ea759d2368d05411617b82236b4356a5c000de37
Minister <mundestephane13@gmail.com>
2026-03-05 01:32:19 +0100

    fix: Minor SEO updates

--- projects/plug/src/app/app.ts
+++ projects/plug/src/app/app.ts
@@ -1,4 +1,12 @@
-import { Component, DOCUMENT, effect, Inject, inject, Renderer2, signal } from "@angular/core";
+import {
+	Component,
+	DOCUMENT,
+	effect,
+	Inject,
+	inject,
+	Renderer2,
+	signal,
+} from "@angular/core";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { Meta } from "@angular/platform-browser";
 import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
@@ -12,97 +20,93 @@ import { environment } from "../environments/environment";
 	styleUrl: "./app.scss",
 })
 export class App {
-  #router = inject(Router)
+	#router = inject(Router);
 	protected readonly title = signal("plug");
 
-
 	readonly cannonical = toSignal(
-    this.#router.events.pipe(
-      filter(e => e instanceof NavigationEnd),
-      map(() => {
-        const urlTree = this.#router.parseUrl(this.#router.url);
+		this.#router.events.pipe(
+			filter((e) => e instanceof NavigationEnd),
+			map(() => {
+				const urlTree = this.#router.parseUrl(this.#router.url);
 
-        urlTree.fragment = null
-        urlTree.queryParams = {}
+				urlTree.fragment = null;
+				urlTree.queryParams = {};
 
-        return `https://plug.${environment.domain}${urlTree.toString().replace(/\/$/, '')}`
-      })
-    )
-  );
+				return `https://plug.${environment.domain}${urlTree.toString().replace(/\/$/, "")}`;
+			}),
+		),
+	);
 
-	constructor(meta: Meta, renderer: Renderer2, @Inject(DOCUMENT) document: Document) {
-    effect(() => {
-      const element = document.querySelector("link[rel='canonical']");
+	constructor(
+		@Inject(Meta) meta: Meta,
+		@Inject(Renderer2) renderer: Renderer2,
+		@Inject(DOCUMENT) document: Document,
+	) {
+		effect(() => {
+			const element = document.querySelector("link[rel='canonical']");
 
-      if (element) {
-        renderer.removeChild(document.head, element)
-      }
+			if (element) {
+				renderer.removeChild(document.head, element);
+			}
 
-      if (this.cannonical()) {
-        const link: HTMLLinkElement = renderer.createElement('link')
-        renderer.setAttribute(link, 'rel', 'canonical');
-        renderer.setAttribute(link, 'href', this.cannonical()!);
-        renderer.appendChild(document.head, link)
-      }
-    })
-    meta.updateTag({
+			if (this.cannonical()) {
+				const link: HTMLLinkElement = renderer.createElement("link");
+				renderer.setAttribute(link, "rel", "canonical");
+				renderer.setAttribute(link, "href", this.cannonical()!);
+				renderer.appendChild(document.head, link);
+			}
+		});
 
-				id: "description",
-				name: "description",
-				content:
-					"A platform that helps students better thier grades and overcome academic challenges",
-
-    })
-    meta.updateTag(
-      {
-        id: "og:description",
-        property: "og:description",
-        content:
-        "A platform that helps students better thier grades and overcome academic challenges",
-      }
-    );
-
-    meta.updateTag(
-      {
-        id: "keywords",
-        name: "keywords",
-        content:
-        "plug, stundz, academic, study, tutorials, past questions, project, revision, education",
-      }
-    )
-    meta.updateTag(
-      {
-        id: "og:image",
-        property: "og:image",
-        content:
-        "https://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVf0U_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
-      }
-    )
-    meta.updateTag(
-      {
-        id: "og:image.url",
-        property: "og:image",
-        content:
-        "http://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVf0U_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
-      }
-    )
-    meta.updateTag({
-      id: "og:image.secure_url",
-      property: "og:image",
-      content:
-      "http://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVfOU_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
-    })
-    meta.updateTag( {
-        id: "og:site_name",
-        property: "og:site_name",
-        content: "Plug",
-    })
-
-    meta.updateTag({
-        id: "og:type",
-        property: "og:type",
-        content: "website",
-    })
+		meta.addTags(
+			[
+				{
+					id: "description",
+					name: "description",
+					content:
+						"A platform that helps students better thier grades and overcome academic challenges",
+				},
+				{
+					id: "keywords",
+					name: "keywords",
+					content:
+						"plug, stundz, academic, study, tutorials, past questions, project, revision, education",
+				},
+				{
+					id: "og:description",
+					property: "og:description",
+					content:
+						"A platform that helps students better thier grades and overcome academic challenges",
+				},
+				{
+					id: "og:image",
+					property: "og:image",
+					content:
+						"https://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVf0U_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
+				},
+				{
+					id: "og:image.url",
+					property: "og:image.url",
+					content:
+						"http://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVf0U_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
+				},
+				{
+					id: "og:image.secure_url",
+					property: "og:image.secure_url",
+					content:
+						"https://lh3.googleusercontent.com/aida-public/AB6AXuDVeHxpk_drXp9EcCmSEJuyY77JIswtRVfOU_jrFGMeXKJfnA2dSZgaQiQwkETC234nHhSIGxEf2eV_-i-FcmzSESBDjapcN4W4oQ62l0UFXAATz-RFvbRn9cljeJ8g6RBJuFztxtoh3vQBBHC2l-ZQfS0Bnh7fQJqLVLK3wI-b_TJpWc8EZVSqxYv5C3w68srJOxHH0OOe6ABsV17Qusv8f9-R2YbYXENCM76RbyBKK1Q65bM8By7pKJ2iPL3Mod6ZsivqTcrXV8oL",
+				},
+				{
+					id: "og:site_name",
+					property: "og:site_name",
+					content: "Plug",
+				},
+				{
+					id: "og:type",
+					property: "og:type",
+					content: "website",
+				},
+			],
+			false,
+		);
 	}
-
 }
