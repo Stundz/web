1c87fa624b27936a80ed2b6ab0dffd1fe6c4e31b
Minister <mundestephane13@gmail.com>
2026-04-09 09:07:05 +0100

    chore(plug): Changed server routes to RenderMode.Server

--- projects/plug/src/app/app.routes.server.ts
+++ projects/plug/src/app/app.routes.server.ts
@@ -1,36 +1,8 @@
-import { RenderMode, ServerRoute } from "@angular/ssr";
+import { RenderMode, type ServerRoute } from "@angular/ssr";
 
 export const serverRoutes: ServerRoute[] = [
 	{
 		path: "**",
-		renderMode: RenderMode.Prerender,
-	},
-	{
-		path: "dashboard",
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: "past-questions",
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: "past-question/:past-question/**",
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: "tutorials",
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: "tutorial/:tutorial/**",
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: "tutors",
-		renderMode: RenderMode.Server,
-	},
-	{
-		path: "tutor/:tutor/**",
 		renderMode: RenderMode.Server,
 	},
 ];
