7f3ce6d546117b518606f4100e46ffb61a0d6dda
Minister <mundestephane13@gmail.com>
2026-04-09 09:10:54 +0100

    chore(auth): Added csrf-interceptor

--- projects/auth/src/app/app.config.ts
+++ projects/auth/src/app/app.config.ts
@@ -1,22 +1,25 @@
 import {
-	ApplicationConfig,
-	provideBrowserGlobalErrorListeners,
-	provideZonelessChangeDetection,
-} from "@angular/core";
-import { provideRouter, withComponentInputBinding } from "@angular/router";
-
-import { routes } from "./app.routes";
-import {
-	provideClientHydration,
-	withEventReplay,
-} from "@angular/platform-browser";
-import {
 	provideHttpClient,
 	withFetch,
 	withInterceptors,
 } from "@angular/common/http";
-import { ENVIRONMENT, stundzInterceptor } from "shared";
+import {
+	type ApplicationConfig,
+	provideBrowserGlobalErrorListeners,
+	provideZonelessChangeDetection,
+} from "@angular/core";
+import {
+	MAT_FORM_FIELD_DEFAULT_OPTIONS,
+	type MatFormFieldDefaultOptions,
+} from "@angular/material/form-field";
+import {
+	provideClientHydration,
+	withEventReplay,
+} from "@angular/platform-browser";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { csrfInterceptor, ENVIRONMENT, stundzInterceptor } from "shared";
 import { environment } from "../environments/environment";
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -24,10 +27,14 @@ export const appConfig: ApplicationConfig = {
 		provideZonelessChangeDetection(),
 		provideRouter(routes, withComponentInputBinding()),
 		provideClientHydration(withEventReplay()),
-		provideHttpClient(withFetch(), withInterceptors([stundzInterceptor])),
+		provideHttpClient(withFetch(), withInterceptors([csrfInterceptor])),
 		{
 			provide: ENVIRONMENT,
 			useValue: environment,
+		},
+		{
+			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+			useValue: { appearance: "outline" } as MatFormFieldDefaultOptions,
 		},
 	],
 };
