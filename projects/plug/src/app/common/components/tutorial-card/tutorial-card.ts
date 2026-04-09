59f61b6d5d60bcae46e4bcbc0090c244cf5463fa
Minister <mundestephane13@gmail.com>
2026-04-09 09:12:00 +0100

    chore(plug): Updated tutorial card component

--- projects/plug/src/app/common/components/tutorial-card/tutorial-card.ts
+++ projects/plug/src/app/common/components/tutorial-card/tutorial-card.ts
@@ -1,15 +1,28 @@
-import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import { DatePipe } from "@angular/common";
+import {
+	ChangeDetectionStrategy,
+	Component,
+	computed,
+	input,
+} from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { RouterLink } from "@angular/router";
+import { differenceInDays } from "date-fns";
 import type { Model } from "shared";
 
 @Component({
 	selector: "plug-tutorial-card",
-	imports: [RouterLink, MatButtonModule],
+	imports: [RouterLink, MatButtonModule, DatePipe],
 	templateUrl: "./tutorial-card.ng.html",
 	styleUrl: "./tutorial-card.css",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TutorialCard {
 	tutorial = input.required<Model.Plug.Tutorial>();
+
+	sessionDateDifference = computed(() =>
+		this.tutorial()?.session
+			? differenceInDays(new Date(), this.tutorial().session!.day)
+			: null,
+	);
 }
