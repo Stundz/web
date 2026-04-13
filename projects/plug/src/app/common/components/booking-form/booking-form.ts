14e6b97fd7b20a2d31205616a04106a6d77a4ba3
Minister <mundestephane13@gmail.com>
2026-04-13 01:49:20 +0100

    chore(plug): Revamped the booking form modal

--- projects/plug/src/app/common/components/booking-form/booking-form.ts
+++ projects/plug/src/app/common/components/booking-form/booking-form.ts
@@ -1,22 +1,51 @@
 import { CurrencyPipe } from "@angular/common";
-import { Component, inject } from "@angular/core";
-import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, signal } from "@angular/core";
+import {
+	FormField,
+	FormRoot,
+	form,
+	pattern,
+	required,
+} from "@angular/forms/signals";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
+import { firstValueFrom, map } from "rxjs";
+import { environment } from "../../../../environments/environment";
 
 @Component({
 	selector: "plug-booking-form",
-	imports: [MatButtonModule, MatInputModule, ReactiveFormsModule, CurrencyPipe],
+	imports: [MatButtonModule, MatInputModule, CurrencyPipe, FormField, FormRoot],
 	templateUrl: "./booking-form.ng.html",
 	styleUrl: "./booking-form.scss",
 })
 export class BookingForm {
-	#fb = inject(FormBuilder);
+	#http = inject(HttpClient);
 
-	form = this.#fb.group({
-		phone: this.#fb.control("", {
-			nonNullable: true,
-			validators: [Validators.required],
+	form = form(
+		signal({
+			phone: "",
 		}),
-	});
+		(root) => {
+			required(root.phone, {
+				message: "The phone number is required",
+				when: ({ stateOf }) => stateOf(root).touched(),
+			});
+			pattern(root.phone, new RegExp(/^6[5789]\d{7}$/), {
+				message:
+					"Number should be a valid MTN/Orange Cameroon number without country code (237)",
+			});
+		},
+		{
+			submission: {
+				action: (tree) => {
+					return firstValueFrom(
+						this.#http
+							.post(`${environment.url.api}/plug/tutorial/book`, tree().value())
+							.pipe(map(() => undefined)),
+					);
+				},
+			},
+		},
+	);
 }
