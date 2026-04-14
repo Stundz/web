a7640d8c267895a6f58aa0ad92596d5c2690606d
Minister <mundestephane13@gmail.com>
2026-04-14 18:59:09 +0100

    chore: User resolver now uses the auth service

--- projects/shared/src/lib/resolvers/user-resolver.ts
+++ projects/shared/src/lib/resolvers/user-resolver.ts
@@ -1,20 +1,10 @@
-import { isPlatformServer } from "@angular/common";
-import { inject, PLATFORM_ID } from "@angular/core";
-import { ResolveFn } from "@angular/router";
-import { catchError, of } from "rxjs";
-import { User } from "../services";
-import { Model } from "../types";
+import { inject } from "@angular/core";
+import type { ResolveFn } from "@angular/router";
+import { Auth } from "../services";
+import type { Model } from "../types";
 
-export const userResolver: ResolveFn<Model.User | undefined> = (
-	route,
-	state,
-) => {
-	const userService = inject(User);
-	const platformId = inject(PLATFORM_ID);
+export const userResolver: ResolveFn<Model.User | null> = (route, state) => {
+	const userService = inject(Auth);
 
-	if (isPlatformServer(platformId)) {
-		return undefined;
-	}
-
-	return userService.user$.pipe(catchError(() => of(undefined)));
+	return userService.user$.pipe();
 };
