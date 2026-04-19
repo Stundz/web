6802b951e9265d0282da896d1100e7eaf8269908
Minister <mundestephane13@gmail.com>
2026-04-19 10:26:02 +0100

    chore(plug): Added screen to view curren user's sessions

---
+++ projects/plug/src/app/my-sessions/my-sessions.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'plug-my-sessions',
+  imports: [],
+  templateUrl: './my-sessions.page.ng.html',
+  styleUrl: './my-sessions.page.css',
+})
+export class MySessionsPage {
+
+}
