190d47824e6498534937f7be7f7963e429538a41
Minister <mundestephane13@gmail.com>
2026-05-07 02:24:43 +0100

    chore: ...

--- projects/premifly/src/app/app.config.ts
+++ projects/premifly/src/app/app.config.ts
@@ -1,12 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import {
+	type ApplicationConfig,
+	provideBrowserGlobalErrorListeners,
+} from "@angular/core";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
+import {
+	provideClientHydration,
+	withEventReplay,
+} from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import { csrfInterceptor, ENVIRONMENT, stundzInterceptor } from "shared";
+import { environment } from "../environments/environment";
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(routes),
+		provideClientHydration(withEventReplay()),
+		provideHttpClient(withInterceptors([stundzInterceptor, csrfInterceptor])),
+		{
+			provide: ENVIRONMENT,
+			useValue: environment,
+		},
+		{
+			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+			useValue: { appearance: "outline" },
+		},
+	],
 };
