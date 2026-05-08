ce8404cf2f1ad7513da23743475d00d991fd4899
Minister <mundestephane13@gmail.com>
2026-05-08 17:00:05 +0100

    chor(premifly): ...

--- projects/premifly/src/app/app.ts
+++ projects/premifly/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { ActivatedRoute, RouterOutlet } from "@angular/router";
+import { map } from "rxjs";
+import type { Model } from "../../../../dist/shared/types/shared";
 
 @Component({
-  selector: 'premifly-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: "premifly-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('premifly');
+	protected readonly title = signal("premifly");
+	user = toSignal(
+		inject(ActivatedRoute).data.pipe(
+			map((data) => (data["user"] as Model.User) || null),
+		),
+		{
+			initialValue: null,
+		},
+	);
 }
