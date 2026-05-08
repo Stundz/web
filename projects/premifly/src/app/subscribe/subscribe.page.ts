e2798cbd11470a25a59601eee076f5ffebf3a4ad
Minister <mundestephane13@gmail.com>
2026-05-08 17:00:27 +0100

    chore(premifly): Updated subscription page

--- projects/premifly/src/app/subscribe/subscribe.page.ts
+++ projects/premifly/src/app/subscribe/subscribe.page.ts
@@ -1,5 +1,11 @@
 import { httpResource } from "@angular/common/http";
-import { Component, signal } from "@angular/core";
+import {
+	Component,
+	computed,
+	input,
+	linkedSignal,
+	signal,
+} from "@angular/core";
 import {
 	email,
 	FormField,
@@ -28,6 +34,7 @@ import { environment } from "../../environments/environment";
 	styleUrl: "./subscribe.page.css",
 })
 export class SubscribePage {
+	user = input.required<Model.User | undefined>();
 	services = httpResource<Array<Model.Premifly.Service>>(
 		() => `${environment.url.api}/premifly/services`,
 		{
@@ -35,13 +42,19 @@ export class SubscribePage {
 		},
 	);
 
-	formModel = signal({
-		first_name: "",
-		last_name: "",
-		email: "",
-		service_id: "",
-		phone_type: "",
-	});
+	formModel = linkedSignal(
+		() => ({
+			first_name: this.user()?.first_name || "",
+			last_name: this.user()?.last_name ?? "",
+			email: this.user()?.email ?? "",
+			service_id: "",
+			phone_type: "",
+			phone: this.user()?.phone ?? "",
+		}),
+		{
+			debugName: "Subscription Form",
+		},
+	);
 
 	form = form(this.formModel, (root) => {
 		required(root.first_name, {
@@ -61,14 +74,33 @@ export class SubscribePage {
 		email(root.email, { message: "Invalid email" });
 
 		required(root.service_id, { message: "Please select a service" });
-		required(root.service_id, { message: "Please select a service" });
+
+		required(root.phone, {
+			message: "Please select a service",
+			when: ({ stateOf }) => stateOf(root).touched(),
+		});
 	});
 
-	accounts = httpResource<Array<Model.Premifly.Account>>(() =>
-		this.formModel().service_id
-			? {
-					url: `${environment.url.api}/premifly/service/${this.formModel().service_id}/accounts`,
-				}
-			: undefined,
+	accounts = httpResource<Array<Model.Premifly.Account>>(
+		() =>
+			this.formModel().service_id
+				? {
+						url: `${environment.url.api}/premifly/service/${this.formModel().service_id}/accounts`,
+					}
+				: undefined,
+		{
+			defaultValue: [],
+		},
+	);
+
+	account = linkedSignal(() =>
+		this.accounts
+			.value()
+			.sort((a, b) => a.subscriptions_count - b.subscriptions_count)
+			.at(0),
+	);
+
+	service = computed(() =>
+		this.services.value().find((s) => s.id === this.form.service_id().value()),
 	);
 }
