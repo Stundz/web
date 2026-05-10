f16e938397d8bfa3464146e64725820d2ab20aaf
Minister <mundestephane13@gmail.com>
2026-05-10 13:22:01 +0100

    chore(premifly): Created service-card

---
+++ projects/premifly/src/app/common/components/service-card/service-card.ts
@@ -0,0 +1,17 @@
+import { CurrencyPipe } from "@angular/common";
+import { Component, input } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import type { Model } from "shared";
+
+@Component({
+	selector: "premifly-service-card",
+	imports: [CurrencyPipe, MatButtonModule],
+	templateUrl: "./service-card.ng.html",
+	styleUrl: "./service-card.css",
+	host: {
+		"[class.service-card]": "true",
+	},
+})
+export class ServiceCard {
+	service = input.required<Model.Premifly.Service>();
+}
