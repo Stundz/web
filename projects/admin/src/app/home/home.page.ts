f802455b2806da62459b5075bca42eac7d5626ec
Minister <mundestephane13@gmail.com>
2026-05-11 23:05:08 +0100

    chore(admin): Created home page for dashboard

---
+++ projects/admin/src/app/home/home.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-home',
+  imports: [],
+  templateUrl: './home.page.ng.html',
+  styleUrl: './home.page.css',
+})
+export class HomePage {
+
+}
