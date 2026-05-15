2ebf2e382a1be6fc49242c35a60c290934b88247
Minister <mundestephane13@gmail.com>
2026-05-15 03:20:11 +0100

    chore(admin): Revamped the entry component

--- projects/admin/src/app/app.ts
+++ projects/admin/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'admin-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: "admin-root",
+	imports: [MatButtonModule, RouterLink, RouterLinkActive, RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.scss",
 })
 export class App {
-  protected readonly title = signal('admin');
+	protected readonly title = signal("admin");
 }
