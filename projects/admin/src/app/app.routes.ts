689358297582e21965fa7862406437290d9c5ef8
Minister <mundestephane13@gmail.com>
2026-05-15 03:20:28 +0100

    chore(admin): Added routes for various stundz apps

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -3,13 +3,20 @@ import { userResolver } from "shared";
 
 export const routes: Routes = [
 	{
+		path: "",
 		resolve: {
 			user: userResolver,
 		},
 		children: [
 			{
 				path: "",
+				pathMatch: "full",
 				loadComponent: () => import("./home/home.page").then((m) => m.HomePage),
+			},
+			{
+				path: "",
+				pathMatch: "prefix",
+				loadChildren: () => import("./apps/apps.routes").then((m) => m.routes),
 			},
 		],
 	},
