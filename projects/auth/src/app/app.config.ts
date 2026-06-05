56706bfe5987ff37135bab80d9997944c470a806
Minister <mundestephane13@gmail.com>
2026-06-05 04:02:23 +0100

    ...

--- projects/auth/src/app/app.config.ts
+++ projects/auth/src/app/app.config.ts
@@ -1,49 +1,44 @@
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import {
-	provideHttpClient,
-	withFetch,
-	withInterceptors,
-} from "@angular/common/http";
-import {
-	type ApplicationConfig,
-	inject,
-	provideAppInitializer,
-	provideBrowserGlobalErrorListeners,
-	provideZonelessChangeDetection,
+  type ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
 } from "@angular/core";
 import {
-	MAT_FORM_FIELD_DEFAULT_OPTIONS,
-	type MatFormFieldDefaultOptions,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  type MatFormFieldDefaultOptions,
 } from "@angular/material/form-field";
 import {
   provideClientHydration,
   withEventReplay,
-  withNoIncrementalHydration
 } from "@angular/platform-browser";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
 import { firstValueFrom } from "rxjs";
-import { Auth, csrfInterceptor, ENVIRONMENT, stundzInterceptor } from "shared";
+import { Auth, csrfInterceptor, ENVIRONMENT } from "shared";
 import { environment } from "../environments/environment";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-		provideBrowserGlobalErrorListeners(),
-		provideZonelessChangeDetection(),
-		provideRouter(routes, withComponentInputBinding()),
-		provideClientHydration(withEventReplay(), withNoIncrementalHydration()),
-		provideHttpClient(withFetch(), withInterceptors([csrfInterceptor])),
-		provideAppInitializer(async () => {
-			const authService = inject(Auth);
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptors([csrfInterceptor])),
+    provideAppInitializer(async () => {
+      const authService = inject(Auth);
 
-			return await firstValueFrom(authService.getUser());
-		}),
-		{
-			provide: ENVIRONMENT,
-			useValue: environment,
-		},
-		{
-			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-			useValue: { appearance: "outline" } as MatFormFieldDefaultOptions,
-		},
-	],
+      return await firstValueFrom(authService.getUser());
+    }),
+    {
+      provide: ENVIRONMENT,
+      useValue: environment,
+    },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: "outline" } as MatFormFieldDefaultOptions,
+    },
+  ],
 };
