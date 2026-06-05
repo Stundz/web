f88c421c08c9b304399743add9d613b107a22e42
Minister <mundestephane13@gmail.com>
2026-06-05 19:58:40 +0100

    chore(premifly): ...

--- projects/premifly/src/app/dashboard/dashboard.page.ts
+++ projects/premifly/src/app/dashboard/dashboard.page.ts
@@ -1,10 +1,28 @@
 import { CurrencyPipe, DatePipe } from "@angular/common";
 import { httpResource } from "@angular/common/http";
-import { Component, input, ChangeDetectionStrategy } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+  linkedSignal,
+  signal,
+} from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { debounce, form } from "@angular/forms/signals";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import {
+  MatPaginatorModule,
+  type PageEvent,
+} from "@angular/material/paginator";
 import { MatTableModule } from "@angular/material/table";
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import {
+  ActivatedRoute,
+  type Params,
+  RouterLink,
+  RouterLinkActive,
+} from "@angular/router";
 import { differenceInDays } from "date-fns";
 import type { Model, Paginated } from "shared";
 import { environment } from "../../environments/environment";
@@ -19,6 +37,7 @@ import { environment } from "../../environments/environment";
     // Angular Material
     MatButtonModule,
     MatCardModule,
+    MatPaginatorModule,
     MatTableModule,
   ],
   templateUrl: "./dashboard.page.ng.html",
@@ -28,18 +47,86 @@ import { environment } from "../../environments/environment";
 export class DashboardPage {
   user = input.required<Model.User>();
 
-  subscriptions = httpResource<Paginated<Model.Premifly.Subscription>>(() => ({
-    url: `${environment.url.api}/premifly/subscriptions`,
-    params: {
-      u: this.user().id,
+  subscriptions = httpResource<Paginated<Model.Premifly.Subscription>>(
+    () => ({
+      url: `${environment.url.api}/premifly/subscriptions`,
+      params: {
+        u: this.user().id,
+      },
+    }),
+    {
+      defaultValue: {
+        data: [] as Array<Model.Premifly.Subscription>,
+        meta: {
+          total: 0,
+          from: 0,
+          to: 0,
+          current_page: 1,
+          per_page: 15,
+        },
+      } as Paginated<Model.Premifly.Subscription>,
     },
-  }));
+  );
 
   date = new Date();
+
+  #route = inject(ActivatedRoute);
+  #queryParams = toSignal(this.#route.queryParams, {
+    initialValue: {} as Params,
+  });
+  stats = httpResource(() => ({
+    url: `${environment.url.api}/premifly/stats`,
+  }));
+  formState = linkedSignal(() => {
+    const params = this.#queryParams();
+
+    return {
+      q: params["q"] || "",
+      status: params["status"] || "",
+      period: params["period"] || "",
+      page: Number(params["page"]) || 1,
+      limit: params["limit"] || null,
+    };
+  });
+
+  form = form(
+    this.formState,
+    (root) => {
+      debounce(root.q, 600);
+    },
+    {},
+  );
+
+  pageSizes = linkedSignal(() => [
+    ...new Set([
+      5,
+      10,
+      15,
+      20,
+      this.subscriptions.value()?.meta?.per_page || 15,
+    ]),
+  ]);
 
   isActiveSubscription(date?: string) {
     if (!date) return false;
 
     return differenceInDays(date, Date.now()) > 0;
+  }
+
+  handlePaginatorEvent(event: PageEvent) {
+    this.form().value.update((value) => {
+      value["page"] = event.pageIndex + 1;
+
+      if (
+        this.subscriptions.value()?.meta?.per_page &&
+        this.subscriptions.value()?.meta.per_page !== event.pageSize
+      ) {
+        value["limit"] = String(event.pageSize);
+      } else {
+        value["limit"] = null;
+      }
+
+      return value;
+    });
   }
 }
