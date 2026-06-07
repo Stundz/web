989bc6f2c0829c27d15771b7149b17f2fadb18de
Minister <mundestephane13@gmail.com>
2026-06-07 22:39:33 +0100

    fix: Revamped the auth-gard

--- projects/shared/src/lib/guards/auth-guard.ts
+++ projects/shared/src/lib/guards/auth-guard.ts
@@ -1,25 +1,39 @@
-import { inject, PLATFORM_ID } from "@angular/core";
-import { CanActivateFn, RedirectCommand, Router } from "@angular/router";
+import { inject } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { User } from "../services";
-import { catchError, map, of } from "rxjs";
-import { isPlatformServer } from "@angular/common";
+import type { CanActivateFn } from "@angular/router";
+import { map, of, switchMap, take } from "rxjs";
+import { Auth } from "../services";
 
 export const authGuard: CanActivateFn = (route, state) => {
-	const snackBar = inject(MatSnackBar);
-	const guestPath = inject(Router).parseUrl("/");
-	const userService = inject(User);
-	const platformId = inject(PLATFORM_ID);
+  const snackBar = inject(MatSnackBar);
 
-	if (isPlatformServer(platformId)) {
-		return true;
-	}
+  const authService = inject(Auth);
 
-	return userService.user$.pipe(
-		map((user) => !!user),
-		catchError(() => {
-			snackBar.open("You are unauthenticated");
-			return of(new RedirectCommand(guestPath));
-		}),
-	);
+  return authService.user$.pipe(
+    take(1),
+    // Take the current state emission
+    switchMap((user) => {
+      console.log(user);
+      // If user state is already in memory, use it
+      if (user !== null && user !== undefined) {
+        return of(true);
+      }
+
+      // If state is null, trigger getUser() to fetch from backend API (critical for initial SSR/Refreshes)
+      if (user === undefined) {
+        return authService.getUser().pipe(
+          map((fetchedUser) => {
+            if (fetchedUser !== null) {
+              return true;
+            }
+
+            snackBar.open("You are unauthenticated");
+            return false;
+          }),
+        );
+      }
+
+      return of(false);
+    }),
+  );
 };
